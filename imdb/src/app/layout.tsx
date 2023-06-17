b772a7a874bb55b1d6cedfd67fb00966e71d2585
Lee Dano <eksh7080@gmail.com>
2023-06-17 17:16:12 +0900

    [FEAT] Nav 추가

--- imdb/src/app/layout.tsx
+++ imdb/src/app/layout.tsx
@@ -1,32 +1,29 @@
-import Header from '@/components/Header'
-import './global.css'
-import Providers from './Providers'
+import Header from '@/components/Header';
+import './global.css';
+import Providers from './Providers';
+import Navbar from '@/components/Navbar';
 
 export const metadata = {
   title: 'INVU',
   description: 'APP',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body>
         <Providers>
-
           {/* Header */}
           <Header />
 
           {/* Navbar */}
+          <Navbar />
 
           {/* SearchBox */}
 
-        {children}
+          {children}
         </Providers>
       </body>
     </html>
-  )
+  );
 }
