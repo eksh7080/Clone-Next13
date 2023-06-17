783af095aa9b925d1b3fe7064d96bca7e7c718c5
Lee Dano <eksh7080@gmail.com>
2023-06-17 19:19:46 +0900

    너무 많다

--- imdb/src/app/loading.tsx
+++ imdb/src/app/loading.tsx
@@ -1,11 +1,9 @@
-import Spinner from '@/components/Spinner';
-
 // 로딩 할 때 해당 페이지 요소가 표시됨
 const loading = () => {
   return (
-    <section className="flex justify-center">
-      <Spinner />
-    </section>
+    <article className="flex justify-center ">
+      <img src="/spinning-circles.svg" alt="spinner" className="h-96" />
+    </article>
   );
 };
 
--- imdb/src/components/Results.tsx
+++ imdb/src/components/Results.tsx
@@ -1,8 +1,10 @@
+import Card from './Card';
+
 const Results = ({ res }) => {
   return (
-    <section>
-      {res.map((db) => (
-        <div key={db.id}>{db.original_title}</div>
+    <section className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
+      {res.map((items) => (
+        <Card items={items} />
       ))}
     </section>
   );
