890d6764566b60ce80b3c18c613f4fb4dbd9918c
Lee Dano <eksh7080@gmail.com>
2023-08-29 22:54:12 +0900

    [FEAT] 구글 검색 엔진 API

--- google/src/app/search/web/page.tsx
+++ google/src/app/search/web/page.tsx
@@ -1,5 +1,12 @@
-const WebSearchPage = () => {
-  return <section>WebSerch</section>;
+const WebSearchPage = async ({ searchParams }) => {
+  const res = await fetch(
+    ` https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`,
+  );
+
+  const data = await res.json();
+  const result = data.items;
+
+  return <>{result && result.map((list) => <h1>{list.title}</h1>)}</>;
 };
 
 export default WebSearchPage;
