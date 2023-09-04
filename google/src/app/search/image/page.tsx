515ec501e647ade729b2c9785b773d05a2a36983
Lee Dano <eksh7080@gmail.com>
2023-09-04 22:20:07 +0900

    [FEAT] 이미지 검색결과 페이지

--- google/src/app/search/image/page.tsx
+++ google/src/app/search/image/page.tsx
@@ -1,5 +1,31 @@
-const ImageSearchPage = () => {
-  return <section>ImageSerch</section>;
+import ImageSearchResults from '@/components/ImageSearchResults';
+import Link from 'next/link';
+
+const ImageSearchPage = async ({ searchParams }) => {
+  const res = await fetch(
+    ` https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`,
+  );
+
+  if (!res.ok) {
+    throw new Error('Something went wrong');
+  }
+
+  const data = await res.json();
+
+  const result = data.items;
+
+  if (!result) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">Try searching for something else or go back to the homepage.</p>
+        <Link href="/" className="text-blue-500">
+          Home
+        </Link>
+      </div>
+    );
+  }
+  return <>{result && <ImageSearchResults result={data} />}</>;
 };
 
 export default ImageSearchPage;
